882be459c70b9403a8621aa3af315fe72c398b10
markjt031 <markjt031@gmail.com>
2023-03-08 20:23:56 -0500

    added model

---
+++ models/project.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const ProjectSchema= new mongoose.schema({
+    name: {type: String, required: true},
+    img: { data: Buffer, contentType: String },
+    description: {type: String},
+    colors: [{type: String}],
+    yarnBrands: [{type: String}],
+    patternCredit: {type: String},
+    complete: {type: Boolean}
+}, {timeStamps: true}, {collection: "Projects" });
+
+const Project=mongoose.model("Project", ProjectSchema);
+module.exports=Project;
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ require('dotenv').config();
 const express=require('express');
 const mongoose=require('mongoose');
 const methodOverride=require('method-override');
+const upload=require('multer');
+const Project=require('./models/project');
+
+
 
 const app=express();
 const PORT=process.env.PORT;
 const mongoURI=process.env.MONGO_URI;
+
+app.use(express.static('public'));
+app.use("/uploads", express.static('uploads'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'));
+
 
 mongoose.connect(mongoURI, {
 	useNewUrlParser: true,
@@ -18,4 +29,24 @@ db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
+
+app.get("/gallery", (req, res)=>{
+    res.send("this is the gallery page");
+})
+
+app.get("/gallery/new", (req, res)=>{
+    res.send("this is the new page");
+})
+
+app.post("/gallery", (req, res)=>{
+    console.log(req.body);
+})
+
+app.get("/gallery/:id", (req,res)=>{
+    res.send("this is the show page");
+})
+
+app.get("/gallery/:id/edit", (req, res)=>{
+    res.send("this is the edit page");
+})
 
