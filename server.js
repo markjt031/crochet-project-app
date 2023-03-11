50579dac5f00b81ebcf057420d0c687e1a13db65
markjt031 <markjt031@gmail.com>
2023-03-11 15:45:18 -0500

    small changes

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const mongoose=require('mongoose');
 
 const projectController=require('./controllers/projects')
 
-
-
-const PORT=process.env.PORT;
-const mongoURI=process.env.MONGO_URI;
-//const mongoURI = 'mongodb://localhost:27017/projects';
-
 const app=express();
 
 app.use(express.static('public'));
 app.use("/uploads", express.static('uploads'));
 app.use("/gallery", projectController);
 
-mongoose.connect(mongoURI, {
-	useNewUrlParser: true,
+
+const PORT=process.env.PORT;
+const MONGODB_URI=process.env.MONGODB_URI;
+//const mongoURI = 'mongodb://localhost:27017/projects';
+mongoose.set('strictQuery', true);
+
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: false,
 	useUnifiedTopology: true
 });
 
@@ -25,6 +25,10 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.get("/", (req, res)=>{
+    res.send('home page');
+})
 
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
 
